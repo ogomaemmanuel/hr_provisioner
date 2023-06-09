4f3371c879a7928e6452922118e3fca0ccb914c8
Emmanuel <ogoma.emmanuel@gmail.com>
2023-06-09 13:20:53 +0300

    Added java files

---
+++ src/main/java/com/ogoma/hr_provisioner/HrProvisionerApplication.java
@@ -0,0 +1,13 @@
+package com.ogoma.hr_provisioner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProvisionerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrProvisionerApplication.class, args);
+    }
+
+}
