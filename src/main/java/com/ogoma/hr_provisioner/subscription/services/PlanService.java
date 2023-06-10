b1565d9e9c711b8d7c0b7326e63d0d348f6721b9
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-10 12:14:33 +0300

    added a plan service

--- src/main/java/com/ogoma/hr_provisioner/subscription/services/PlanService.java
+++ src/main/java/com/ogoma/hr_provisioner/subscription/services/PlanService.java
@@ -1,7 +1,14 @@
 package com.ogoma.hr_provisioner.subscription.services;
 
+import com.ogoma.hr_provisioner.subscription.repositories.PlanRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SubscriptionService {
+public class PlanService {
+    private final PlanRepository planRepository;
+
+    public PlanService(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
 }
+
