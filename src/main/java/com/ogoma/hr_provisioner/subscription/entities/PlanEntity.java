f871eb9832b8bd50d3e73bc24bf629a706d707bb
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-10 12:14:01 +0300

    added a plan entity

--- src/main/java/com/ogoma/hr_provisioner/subscription/entities/PlanEntity.java
+++ src/main/java/com/ogoma/hr_provisioner/subscription/entities/PlanEntity.java
@@ -1,2 +1,15 @@
-package com.ogoma.hr_provisioner.subscription.entities;public class PlanEntity {
+package com.ogoma.hr_provisioner.subscription.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class PlanEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
