c8dff16d9419be87267ba8e552a9b063e30af46d
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-13 13:36:36 +0300

    changed up the endpoint nam

--- src/main/java/com/ogoma/hr_provisioner/plans/controllers/PlanController.java
+++ src/main/java/com/ogoma/hr_provisioner/plans/controllers/PlanController.java
@@ -1,15 +1,20 @@
 package com.ogoma.hr_provisioner.plans.controllers;
 
+import com.ogoma.hr_provisioner.plans.entities.PlanEntity;
 import com.ogoma.hr_provisioner.plans.services.PlanService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/subscription")
+@RequestMapping("/plans")
 public class PlanController {
     private final PlanService planService;
 
     public PlanController(PlanService planService) {
         this.planService = planService;
     }
+
 }
