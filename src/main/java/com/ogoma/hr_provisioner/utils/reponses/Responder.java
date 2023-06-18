b20dd78d1d96204caa845a519df180fcccaee081
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-18 22:10:36 +0300

    created a responder to be used

--- src/main/java/com/ogoma/hr_provisioner/utils/reponses/Responder.java
+++ src/main/java/com/ogoma/hr_provisioner/utils/reponses/Responder.java
@@ -1,2 +1,17 @@
-package com.ogoma.hr_provisioner.utils.reponses;public class Responder {
+package com.ogoma.hr_provisioner.utils.reponses;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Responder {
+    private HashMap<Object, Object> returnObj = new HashMap<>();
+
+    public HashMap<Object, Object> getReturnObj() {
+        return returnObj;
+    }
+
+    public void setReturnObj(Object key, Object value) {
+        this.returnObj.put(key,value);
+    }
 }
