d7224f60ced8a409ce480da78464a4c4d51bbc1d
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-18 22:09:10 +0300

    added a query dto for plans

--- src/main/java/com/ogoma/hr_provisioner/plans/PlanQuery.java
+++ src/main/java/com/ogoma/hr_provisioner/plans/PlanQuery.java
@@ -1,2 +1,60 @@
-package com.ogoma.hr_provisioner.plans;public class PlanQuery {
+package com.ogoma.hr_provisioner.plans;
+
+import com.ogoma.hr_provisioner.plans.entities.PlanEntity;
+import com.ogoma.hr_provisioner.plans.enums.Type;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
+public class PlanQuery {
+    private boolean archive;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    public boolean isArchive() {
+        return archive;
+    }
+
+    public void setArchive(boolean archive) {
+        this.archive = archive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Specification<PlanEntity> toSpec() {
+        return new Specification<PlanEntity>() {
+            @Override
+            public Predicate toPredicate(Root<PlanEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+                Predicate p = criteriaBuilder.conjunction();
+                if (name != null) {
+                    p = criteriaBuilder.and(p, criteriaBuilder.like(root.get("name"), name + "%"));
+                }
+                if (type != null) {
+                    p = criteriaBuilder.and(p, criteriaBuilder.equal(root.get("type"), type));
+                }
+                p = criteriaBuilder.and(p,criteriaBuilder.equal(root.get("archive"),archive));
+                return p;
+            }
+        };
+    }
 }
