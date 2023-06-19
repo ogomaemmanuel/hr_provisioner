4d6e92064708fb6197df087b23e382b81efe2525
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-19 07:45:01 +0300

    extended baserepository

--- src/main/java/com/ogoma/hr_provisioner/plans/repositories/PlanRepository.java
+++ src/main/java/com/ogoma/hr_provisioner/plans/repositories/PlanRepository.java
@@ -1,10 +1,11 @@
 package com.ogoma.hr_provisioner.plans.repositories;
 
+import com.ogoma.hr_provisioner.base.BaseRepository;
 import com.ogoma.hr_provisioner.plans.entities.PlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanRepository extends JpaRepository<PlanEntity, Long> {
+public interface PlanRepository extends BaseRepository<PlanEntity> {
 
 }
