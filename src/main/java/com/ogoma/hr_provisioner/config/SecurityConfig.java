01f79b0d4b60e01b7b9908765a056c5178d0636e
emmanuelogoma <eogoma@safaricom.co.ke>
2023-06-20 14:17:06 +0300

    Minor changes

---
+++ src/main/java/com/ogoma/hr_provisioner/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.ogoma.hr_provisioner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity security){
+
+    }
+}
