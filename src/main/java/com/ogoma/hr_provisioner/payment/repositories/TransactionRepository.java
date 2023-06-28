9c40ed935cf0344ab525d0f568d6d06d209d43b7
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-28 13:08:32 +0300

    added repostiory to handle db querie

--- src/main/java/com/ogoma/hr_provisioner/payment/repositories/TransactionRepository.java
+++ src/main/java/com/ogoma/hr_provisioner/payment/repositories/TransactionRepository.java
@@ -1,2 +1,9 @@
-package com.ogoma.hr_provisioner.payment.repositories;public class TransactionRepository {
+package com.ogoma.hr_provisioner.payment.repositories;
+
+import com.ogoma.hr_provisioner.base.BaseRepository;
+import com.ogoma.hr_provisioner.payment.entities.TransactionEntity;
+
+public interface TransactionRepository extends BaseRepository<TransactionEntity> {
+
+    public TransactionEntity findByMerchantRequestIDAndCheckoutRequestIDAndArchive(String merchantRequestId,String checkoutRequestId,boolean archive);
 }
--- src/main/java/com/ogoma/hr_provisioner/subscriptions/repositories/SubscriptionRepository.java
+++ src/main/java/com/ogoma/hr_provisioner/subscriptions/repositories/SubscriptionRepository.java
@@ -1,2 +1,13 @@
-package com.ogoma.hr_provisioner.subscriptions.repositories;public interface SubscriptionRepository {
+package com.ogoma.hr_provisioner.subscriptions.repositories;
+
+import com.ogoma.hr_provisioner.base.BaseRepository;
+import com.ogoma.hr_provisioner.subscriptions.entities.SubscriptionEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionRepository extends BaseRepository<SubscriptionEntity> {
+   public Optional<SubscriptionEntity> findTopByEmailAndArchiveOrderByCreatedAtDesc(String email, boolean archive);
+   public Optional<SubscriptionEntity> findTopByEmailOrderByCreatedAtDesc(String email);
 }
