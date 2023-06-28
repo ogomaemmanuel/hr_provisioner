5b44a2f38a71115ef512c187b9a0b5dc048e6293
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-28 13:07:01 +0300

    payment dto

--- src/main/java/com/ogoma/hr_provisioner/payment/dtos/PaymentDTO.java
+++ src/main/java/com/ogoma/hr_provisioner/payment/dtos/PaymentDTO.java
@@ -1,2 +1,22 @@
-package com.ogoma.hr_provisioner.payment.dtos;public class PaymentDTO {
+package com.ogoma.hr_provisioner.payment.dtos;
+
+public class PaymentDTO {
+    private String phoneNumber;
+    private Long subscriptionId;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(Long subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
 }
