81ec4bc7445745c2d00a497a77d8bce0b5c4429b
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-28 13:11:26 +0300

    added trans controller

--- src/main/java/com/ogoma/hr_provisioner/payment/controller/TransactionController.java
+++ src/main/java/com/ogoma/hr_provisioner/payment/controller/TransactionController.java
@@ -1,21 +1,15 @@
 package com.ogoma.hr_provisioner.payment.controller;
 
-import com.google.gson.Gson;
 import com.ogoma.hr_provisioner.payment.dtos.PaymentDTO;
 import com.ogoma.hr_provisioner.payment.service.TransactionService;
-import com.ogoma.hr_provisioner.plans.entities.PlanEntity;
-import com.ogoma.hr_provisioner.subscriptions.dtos.MpesaResponseDTO;
+import com.ogoma.hr_provisioner.payment.dtos.MpesaResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
 
 import static com.ogoma.hr_provisioner.utils.helpers.FormatStatusCode.formatStatusCode;
 
