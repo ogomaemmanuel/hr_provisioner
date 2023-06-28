65ad3d2c7076f4294e4c11ae295f941716384702
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-06-28 13:18:24 +0300

    unrestricated endpoints

--- src/main/java/com/ogoma/hr_provisioner/config/ApplicationSecurityConfig.java
+++ src/main/java/com/ogoma/hr_provisioner/config/ApplicationSecurityConfig.java
@@ -6,22 +6,28 @@ import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ApplicationSecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/plans/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/payments/**").permitAll()
+                        .requestMatchers("/subscriptions/**").permitAll()
+                        .anyRequest().permitAll()
                 )
-                .cors(cors->cors.disable())
-                .csrf(csrf->csrf.disable());
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
 }
+
+
